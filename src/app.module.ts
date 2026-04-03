85a6b630594fc8638bcdc2fb42071c5774bfa232
Maxime ELIAZORD <mzozord@gmail.com>
2026-04-03 22:33:16 +0200

    Added api endoint to test pricefeed and expose price

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GainsController } from './controllers/gains.controller';
+import { GnsPriceFeedListenerService } from './services/GnsPriceFeedListenerService';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, GainsController],
+  providers: [AppService, GnsPriceFeedListenerService],
 })
 export class AppModule {}
---
+++ src/controllers/gains.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { GnsPriceFeedListenerService } from '../services/GnsPriceFeedListenerService';
+
+@Controller('gains')
+export class GainsController {
+  constructor(
+    private readonly priceFeedService: GnsPriceFeedListenerService,
+  ) {}
+
+  @Get('price/:pairIndex')
+  getPairPrice(@Param('pairIndex', ParseIntPipe) pairIndex: number) {
+    return this.priceFeedService.getPairPrice(pairIndex);
+  }
+}
